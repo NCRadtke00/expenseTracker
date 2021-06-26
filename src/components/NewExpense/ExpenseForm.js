b5e4c0728796615af5ac2e81b17bfca9f2054763
NCRadtke00 <raddnc@gmail.com>
2021-06-26 16:23:37 -0500

    showing how react allows for different ways of managing state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './ExpenseForm.css'
 
 function ExpenseForm() {
+    //either way works for managing state but doing it in one can be error prone.
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+//    const [userInput, setUserInput] = useState({
+//         enteredTitle: '',
+//         enteredAmount:'',
+//         enteredDate:'',
+//     })
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // }) 
+        //below is needed if we depend on prevState
+        // setUserInput((prevState)=>{
+        //     return {...prevState, enteredTitle: event.target.value}
+        // });
+    };
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // })
+    };
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // })
+    };
+
     return (
         <form>
             <div className="new-expense__controls">
 
                 <div className="new-expense__control">
-                    <label>Title</label>
-                    <input type='text' />
+                    <label>Title: </label>
+                    <input type='text' onChange={titleChangeHandler}/>
                 </div>
 
                 <div className="new-expense__control">
-                    <label>Amount</label>
-                    <input type='numer' min="0.01" step="0.01" />
+                    <label>Amount: </label>
+                    <input type='numer' min="0.01" step="0.01" onChange={amountChangeHandler}/>
                 </div>
 
                 <div className="new-expense__control">
-                    <label> date </label>
-                    <input type='date' min="2019-01-01" max="2022-12-31" />
+                    <label>Date: </label>
+                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
                 </div>
                 <div className="new-expense__actions">
                     <button type="submit">New Expense</button>
