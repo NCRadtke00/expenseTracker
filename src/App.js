33fe5e1930504aa09f2955c5eb7e4985d19c9789
NCRadtke00 <raddnc@gmail.com>
2021-06-26 14:47:16 -0500

    replaced function with const and arrow functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Expenses from "./components/Expenses/Expenses"
-function App() {
+
+const App = () => {
   const expenses = [
     {
       id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14),
