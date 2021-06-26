48e7eadcfcb0fefa20e4d44a46b21225ba94f5da
NCRadtke00 <raddnc@gmail.com>
2021-06-26 16:45:07 -0500

    clearning form upon submition, form is still not done, its currently not saving new expenses. thats okay im going to go workout and finish this when I get done

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 function ExpenseForm() {
@@ -6,11 +6,12 @@ function ExpenseForm() {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-//    const [userInput, setUserInput] = useState({
-//         enteredTitle: '',
-//         enteredAmount:'',
-//         enteredDate:'',
-//     })
+    //    const [userInput, setUserInput] = useState({
+    //         enteredTitle: '',
+    //         enteredAmount:'',
+    //         enteredDate:'',
+    //     })
+ 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
 
@@ -36,25 +37,38 @@ function ExpenseForm() {
         //     ...userInput,
         //     enteredDate: event.target.value,
         // })
+    };  
+     //Two-Way Binding Form
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
 
                 <div className="new-expense__control">
                     <label>Title: </label>
-                    <input type='text' onChange={titleChangeHandler}/>
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Amount: </label>
-                    <input type='numer' min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input type='numer' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Date: </label>
-                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                    <input type='date' min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
                 </div>
                 <div className="new-expense__actions">
                     <button type="submit">New Expense</button>
