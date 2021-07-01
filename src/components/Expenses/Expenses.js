4cc205dcea86db0deb255bc7b6da2b3e0715398b
NCRadtke00 <raddnc@gmail.com>
2021-06-30 23:17:21 -0500

    finished expense tracker, not the best styling and new expense dont save if closed, would need a db

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 //created controlled component meaning its passes on to parent component
@@ -24,6 +25,7 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpensesChart expenses={filteredExpenses}/>
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
