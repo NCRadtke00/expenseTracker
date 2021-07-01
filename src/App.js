78acee3a9ba63ca6e78ff3766f77c15229a1cd36
NCRadtke00 <raddnc@gmail.com>
2021-06-30 22:33:35 -0500

    finished filter, now displays selected year, however not current year. also made form for adding new expense hidden unless clicked. gave option to close form if not needed, and will close upon submitting new expense

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+import React, { useState } from 'react'
 import Expenses from "./components/Expenses/Expenses"
 import NewExpense from "./components/NewExpense/NewExpense"
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+//this is props concept, super important. props = properties that we set. we want to store data here so components can be completely reuseable 
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)
-    },
-    {
-      id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4', title: 'New Desk (Wooden)', amount: 434.67, date: new Date(2021, 5, 12),
-    },
-  ]; //this is props concept, super important. props = properties that we set. we want to store data here so components can be completely reuseable 
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
   //old way of doing react projects 
   // return React.createElement(
   //   'div',
@@ -26,13 +46,13 @@ const App = () => {
   //   React.createElement('h2', {}, "Let's get started!"),
   //   React.createElement(Expenses, { items: expenses })
   // );
-  
+
   return (
-    <div className="App">
-      <NewExpense onAddExpense = {addExpenseHandler}/>
-      <Expenses items={expenses}/>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import './Expenses.css';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem'
-import ExpensesFilter from '../ExpenseFilter/ExpensesFilter';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
 
 //created controlled component meaning its passes on to parent component
 //state vs stateless component (smart vs dumb) typically will have more "dumb" components
@@ -11,19 +11,20 @@ import ExpensesFilter from '../ExpenseFilter/ExpensesFilter';
 
 
 const Expenses = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2020');
-    const filterChangeHandler = selectedYear => {
-        setFilteredYear(selectedYear)
-    }
+    const [filteredYear, setFilteredYear] = useState('2021');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear
+    });
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}
-                {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} /> */}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     )
